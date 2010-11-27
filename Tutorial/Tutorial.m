85ade513145840caadaab7ccc10ba7cd05197d6f
Patrick Russell <patrickrussell100@gmail.com>
2010-11-27 12:36:12 +0000

    First add of interim report

--- Tutorial/Tutorial.m
+++ Tutorial/Tutorial.m
@@ -1,11 +1,45 @@
 #import <Foundation/Foundation.h>
-
-int main (int argc, const char * argv[]) {
-    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
-
-    // insert code here...
-    NSLog(@"Hello, \n World!");
+// --------Interface----------
+@interface Person: NSObject{
+	int age;
+	int weight;
+}
+-(void) print;
+-(void) setAge: (int) a;	
+-(void) setWeight: (int) w;
+-(int) age;
+-(int) weight;
+@end
+// -----------Implementation--------
+@implementation Person
+-(void) print {
+	NSLog(@"I am %i years old and weigh %i pounds", age, weight);
+}
+-(void) setAge: (int) a{
+	age=a;
+}
+-(void) setWeight: (int) w{
+	weight=w;
+}
+-(int) age{
+	return age;
+}
+-(int) weight{
+	return weight;
+}
+@end
+//----------main program ---------------
+int main(int argc, char *argV[]){
+	NSAutoreleasePool * pool = [[NSAutoreleasePool alloc]init];
 	
-    [pool drain];
-    return 0;
+	int i , userNum;
+	NSLog(@"Enter a number and I will print it out");
+	scanf("%i", &userNum);
+	
+	for(i = 1; i<=userNum; i++)
+		NSLog(@"%i", i);
+		
+	
+	[pool drain];
+	return 0;
 }
